cf1ba0f2fd2228fafb478dc5bede2bf78c5399af
MCCGonzalez <m.gonzalez60@ufromail.cl>
2022-08-24 10:36:07 -0400

    cuarto commit

--- src/main/java/CreacionYUsoDeMetodos.java
+++ src/main/java/CreacionYUsoDeMetodos.java
@@ -14,10 +14,11 @@ public class CreacionYUsoDeMetodos {
 
         Bienvenida();
 
-        int numeroMayorCero = entrada();
-        boolean esVerdad = validacion(numeroMayorCero);
-        int numeroDeLibros = CantidadDeLibros(esVerdad, contador);
+        int numeroDeLibros = cuantosLibrosDeseaAgreagar();
         char libros[][] = CrearColeccionDeLibros(numeroDeLibros);
+        String autor = AgregarAutor();
+        String titulo = AgregarTitulo();
+        String editorial = AgregarEditorial();
         char libro[] = BuscarLibro(libros);
         MostrarLibro();
         MostrarEspaciosUsados();
@@ -32,45 +33,43 @@ public class CreacionYUsoDeMetodos {
         System.out.println("Bienvenido a mi programa, para empezar ingresar un libro ingrese un numero mayor a 0");
     }
 
-    private static int entrada(){
+    private static int cuantosLibrosDeseaAgreagar(){
 
         Scanner teclado = new Scanner(System.in);
-        int numeroMayorCero = Integer.valueOf(teclado.nextLine());
+        int numeroDeLibros = Integer.valueOf(teclado.nextLine());
 
-        return numeroMayorCero;
-    }
-
-    private static boolean validacion(int numeroMayorCero) {
-
-        boolean esVerdad = false;
-        if (numeroMayorCero > 0) {
-
-            esVerdad = true;
-
-        }
-        return esVerdad;
-    }
-
-    private static int CantidadDeLibros(boolean esVerdad, int contador) {
-
-        int numeroDeLibros = 0;
-        if (esVerdad = true) {
-
-            numeroDeLibros = contador + 1;
-
-        }
         return numeroDeLibros;
     }
 
-
     private static char[][] CrearColeccionDeLibros(int numeroDeLibros){
 
+        return new char [3][numeroDeLibros];
 
-
-
-        return new char[100][3];
     }
 
+    private static String  AgregarAutor(){
+
+        Scanner teclado = new Scanner(System.in);
+        String autor = teclado.nextLine();
+
+        return autor;
+    }
+
+    private static String AgregarTitulo(){
+
+        Scanner teclado = new Scanner(System.in);
+        String titulo = teclado.nextLine();
+
+        return titulo;
+    }
+
+    private static String AgregarEditorial(){
+
+        Scanner teclado = new Scanner(System.in);
+        String editorial = teclado.nextLine();
+
+        return editorial;
+    }
 
     private static char[] BuscarLibro(char[][] libros){
 
