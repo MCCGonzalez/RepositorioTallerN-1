198e4e3cff14242bfe4c001b2aca838445ac07d7
MCCGonzalez <m.gonzalez60@ufromail.cl>
2022-08-24 09:37:05 -0400

    Segundo Commit

--- src/main/java/CreacionYUsoDeMetodos.java
+++ src/main/java/CreacionYUsoDeMetodos.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CreacionYUsoDeMetodos {
 
     public static void main(String [] args){
@@ -8,19 +10,56 @@ public class CreacionYUsoDeMetodos {
 
     public static void ColeccionDeLibros(){
 
+       String deseaAgregarLibro = entrada();
        char libros[][] = CrearColeccionDeLibros();
-       buscarLibro(libros);
-       
+       char libro[] = BuscarLibro(libros);
+       MostrarLibro();
+       MostrarEspaciosUsados();
+       MostrarEspaciosDisponibles();
+       MostrarTodaLaColeccion();
+
+
     }
+    private static String entrada(){
+
+        Scanner teclado = new Scanner(System.in);
+        String deseaAgregarLibro = teclado.nextLine();
+
+        return deseaAgregarLibro;
+    }
+
 
     private static char[][] CrearColeccionDeLibros(){
 
         return new char[100][3];
     }
+    
 
-    private static void buscarLibro(char[][] libros){
+    private static char[] BuscarLibro(char[][] libros){
+
+         return new char[1];
+    }
+
+    private static void MostrarLibro(){
 
 
     }
+
+    private static void MostrarEspaciosUsados(){
+
+
+    }
+
+    private static void MostrarEspaciosDisponibles(){
+
+
+    }
+
+    private static void MostrarTodaLaColeccion(){
+
+
+
+    }
+
 
 }
