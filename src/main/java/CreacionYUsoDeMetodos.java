5410483b89483d99efa3ca6b280d5310b8969997
MCCGonzalez <m.gonzalez60@ufromail.cl>
2022-08-24 10:14:07 -0400

    tercer commit

--- src/main/java/CreacionYUsoDeMetodos.java
+++ src/main/java/CreacionYUsoDeMetodos.java
@@ -10,30 +10,67 @@ public class CreacionYUsoDeMetodos {
 
     public static void ColeccionDeLibros(){
 
-       String deseaAgregarLibro = entrada();
-       char libros[][] = CrearColeccionDeLibros();
-       char libro[] = BuscarLibro(libros);
-       MostrarLibro();
-       MostrarEspaciosUsados();
-       MostrarEspaciosDisponibles();
-       MostrarTodaLaColeccion();
+        int contador = 0;
+
+        Bienvenida();
+
+        int numeroMayorCero = entrada();
+        boolean esVerdad = validacion(numeroMayorCero);
+        int numeroDeLibros = CantidadDeLibros(esVerdad, contador);
+        char libros[][] = CrearColeccionDeLibros(numeroDeLibros);
+        char libro[] = BuscarLibro(libros);
+        MostrarLibro();
+        MostrarEspaciosUsados();
+        MostrarEspaciosDisponibles();
+        MostrarTodaLaColeccion();
+
 
 
     }
-    private static String entrada(){
+    private static void  Bienvenida(){
+
+        System.out.println("Bienvenido a mi programa, para empezar ingresar un libro ingrese un numero mayor a 0");
+    }
+
+    private static int entrada(){
 
         Scanner teclado = new Scanner(System.in);
-        String deseaAgregarLibro = teclado.nextLine();
+        int numeroMayorCero = Integer.valueOf(teclado.nextLine());
 
-        return deseaAgregarLibro;
+        return numeroMayorCero;
+    }
+
+    private static boolean validacion(int numeroMayorCero) {
+
+        boolean esVerdad = false;
+        if (numeroMayorCero > 0) {
+
+            esVerdad = true;
+
+        }
+        return esVerdad;
+    }
+
+    private static int CantidadDeLibros(boolean esVerdad, int contador) {
+
+        int numeroDeLibros = 0;
+        if (esVerdad = true) {
+
+            numeroDeLibros = contador + 1;
+
+        }
+        return numeroDeLibros;
     }
 
 
-    private static char[][] CrearColeccionDeLibros(){
+    private static char[][] CrearColeccionDeLibros(int numeroDeLibros){
+
+
+
 
         return new char[100][3];
     }
-    
+
 
     private static char[] BuscarLibro(char[][] libros){
 
